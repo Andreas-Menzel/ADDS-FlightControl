336e83406161cfc16ff64a3e7261ee6fb440b0ea
Andreas Menzel <mail@andreas-menzel.com>
2023-01-21 13:21:29 +0100

    Add map visualisation and demo corridors

--- code/flaskr/schema.sql
+++ code/flaskr/schema.sql
@@ -28,3 +28,25 @@ CREATE TABLE corridors (
     intersection_a  TEXT    NOT NULL,
     intersection_b  TEXT    NOT NULL
 );
+
+INSERT INTO intersections VALUES("EDMR-Landeplatz", "48.048121", "11.653678", 0);
+INSERT INTO intersections VALUES("int_1", "48.047705", "11.653841", 0);
+INSERT INTO intersections VALUES("int_2", "48.047679", "11.652243", 0);
+INSERT INTO intersections VALUES("int_3", "48.046641", "11.652629", 0);
+INSERT INTO intersections VALUES("int_4", "48.046800", "11.654028", 0);
+INSERT INTO intersections VALUES("int_5", "48.046876", "11.655620", 0);
+INSERT INTO intersections VALUES("int_6", "48.046928", "11.657085", 0);
+INSERT INTO intersections VALUES("int_7", "48.047849", "11.656265", 0);
+
+INSERT INTO corridors VALUES("cor_1", "EDMR-Landeplatz", "int_1");
+INSERT INTO corridors VALUES("cor_2", "int_1", "int_2");
+INSERT INTO corridors VALUES("cor_3", "int_2", "int_3");
+INSERT INTO corridors VALUES("cor_4", "int_3", "int_4");
+INSERT INTO corridors VALUES("cor_5", "int_4", "int_1");
+INSERT INTO corridors VALUES("cor_6", "int_4", "int_5");
+INSERT INTO corridors VALUES("cor_7", "int_5", "int_6");
+INSERT INTO corridors VALUES("cor_8", "int_6", "int_7");
+INSERT INTO corridors VALUES("cor_9", "int_5", "int_7");
+INSERT INTO corridors VALUES("cor_10", "int_1", "int_7");
+INSERT INTO corridors VALUES("cor_11", "int_1", "int_5");
+INSERT INTO corridors VALUES("cor_12", "int_4", "int_7");
