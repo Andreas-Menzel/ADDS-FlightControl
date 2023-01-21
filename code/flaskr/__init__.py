ac92ee0afc127e41bcd6886e0e1b3ff8a9a116cc
Andreas Menzel <mail@andreas-menzel.com>
2023-01-21 13:20:54 +0100

    Remove unnecessary test code

--- code/flaskr/__init__.py
+++ code/flaskr/__init__.py
@@ -23,11 +23,6 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
     
     from . import db
     db.init_app(app)
